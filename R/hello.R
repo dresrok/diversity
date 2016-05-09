577e0f7292712039aa3375bafaf1dc29fd729322
Andrés Felipe Santos <andresfesantos@gmail.com>
2016-05-08 23:00:58 -0500

    Probando getIVI

--- R/hello.R
+++ R/hello.R
@@ -19,3 +19,25 @@ hello <- function(n1) {
   resultado <- fromJSON("https://api.github.com/users/hadley/orgs")
   print(resultado$id)
 }
+
+getIv <- function(){
+  data <- fromJSON("data/parcela.json")
+  forest <- data.frame(table(data$Especie), stringsAsFactors=FALSE, row.names = NULL);
+  names(forest) <- c('Especie', 'abunA');
+  forest$Especie <- as.character(forest$Especie);
+  forest$abunR <- (forest$abunA/sum(forest$abunA))*100;
+  freqSpecies <- data.frame(table(data$Subparcela, data$Especie));
+  names(freqSpecies) <- c('subparcela', 'especie', 'frecA');
+  dtFreq <- data.frame(table(freqSpecies$especie[freqSpecies$frec > 0]));
+  forest$frecA <- dtFreq$Freq;
+  forest$frecR <- (forest$frecA/sum(forest$frecA))*100;
+  data$dA <- (pi/40000) * (as.numeric(data$Dn)^2);
+  dtDn <- data.frame(tapply(data$dA, data$Especie, sum), row.names=NULL);
+  names(dtDn) <- c('sumDn');
+  forest$domA <-  dtDn$sumDn;
+  forest$domR <- (forest$domA/sum(forest$domA))*100;
+  forest$IVI <- forest$abunR + forest$frecR + forest$domR;
+  total <- data.frame(Especie = "Total", abunA = sum(forest$abunA), abunR = sum(forest$abunR), frecA = sum(forest$frecA), frecR = sum(forest$frecR), domA = sum(forest$domA), domR = sum(forest$domR), IVI = sum(forest$IVI));
+  forest <- rbind(forest, total);
+  return(forest);
+}
